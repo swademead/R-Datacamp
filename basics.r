90f3594ff3e371d7541d441d3749cf43013ce94b
swademead <swademead@gmail.com>
2018-02-22 07:12:54 -0500

    update basics

--- basics.r
+++ basics.r
@@ -262,3 +262,23 @@ for (li in linkedin) {
   
   print(li)
 }
+
+# Pre-defined variables
+rquote <- "r's internals are irrefutably intriguing"
+chars <- strsplit(rquote, split = "")[[1]]
+
+# Initialize rcount
+rcount <- 0
+
+# Finish the for loop
+for (char in chars) {
+  if (char == "r") {
+    rcount <- rcount + 1
+  }
+  if (char == "u") {
+    break
+  }
+}
+
+# Print out rcount
+print(rcount)
