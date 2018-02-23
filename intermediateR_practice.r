4acae9c776c973721b586c7f10eea3b0cb9500ad
swademead <swademead@gmail.com>
2018-02-23 08:49:50 -0500

    update practice

--- intermediateR_practice.r
+++ intermediateR_practice.r
@@ -116,3 +116,83 @@ if(length(top_grades) > length(worst_grades)) {
 }
 
 #CHAPTER 2
+# logs is already available in your workspace
+
+# Print the structure of logs
+str(logs)
+
+# Use list subsetting to print the details part of 11th logs entry
+logs[[11]]$details
+
+# Print the class of the timestamp component of the first entry
+class(logs[[1]]$timestamp)
+
+# logs is available in your workspace
+
+# Initialize the iterator i to be 1
+i = 1
+
+# Code the while loop
+while (logs[[i]]$success == TRUE) {
+  print(i)
+  i <- i + 1
+}
+
+# logs is available in your workspace
+
+# Adapt the while loop
+i <- 1
+while (logs[[i]]$success) {
+  print(logs[[i]]$details$message)
+  i <- i + 1
+}
+
+# Initialize i and found
+i <- 1
+found <- FALSE
+
+# Code the while loop
+while (!found) {
+  if (logs[[i]]$success == FALSE && logs[[i]]$details$location == "waste") {
+    print("found")
+    found <- TRUE
+  } else {
+    print("still looking")
+    i <- i + 1
+  }
+}
+
+# logs is available in your workspace
+
+# Code a for loop that prints the timestamp of each log
+for (log in logs) {
+  print(log$timestamp)
+}
+
+# Make the printout conditional: only if success
+for (log in logs) {
+  if (log$success) {
+    print(log$timestamp)
+  }
+}
+
+# Finish the for loop: add date element for each entry
+for (i in 1:length(logs)) {
+  logs[[i]]$date <- as.Date(logs[[i]]$timestamp)
+}
+
+# Print first 6 elements in logs
+head(logs)
+
+# Intialize empty list: failures
+failures <- list()
+
+# Finish the for loop: add each failure to failures
+for (log in logs) {
+  if (!log$success) {
+    failures <- c(failures, list(log))
+  }
+}
+
+# Display the structure of failures
+str(failures)
