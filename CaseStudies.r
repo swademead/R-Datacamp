ef1187cd1cb24c45382032e644090dbaadc442e8
swademead <swademead@gmail.com>
2018-02-25 17:14:43 -0500

    added chapter 2

--- CaseStudies.r
+++ CaseStudies.r
@@ -81,3 +81,70 @@ sales6 <- unite(sales5, venue_city_state, venue_city, venue_state, sep = ", ")
 
 # View the head of sales6
 head(sales6)
+
+#CHAPTER 2
+# Load readxl
+library(readxl)
+
+# Import mbta.xlsx and skip first row: mbta
+mbta <- read_excel("mbta.xlsx", skip = 1)
+
+# View the structure of mbta
+str(mbta)
+
+# View the first 6 rows of mbta
+head(mbta)
+
+# View a summary of mbta
+summary(mbta)
+
+# Load tidyr
+library(tidyr)
+
+# Gather columns of mbta3: mbta4
+mbta4 <- gather(mbta3, month, thou_riders, -mode)
+
+# View the head of mbta4
+head(mbta4)
+
+# Coerce thou_riders to numeric
+mbta4$thou_riders <- sapply(mbta4$thou_riders, as.numeric)
+
+# Spread the contents of mbta4: mbta5
+mbta5 <- spread(mbta4, mode, thou_riders)
+
+# View the head of mbta5
+head(mbta5)
+
+# View the head of mbta5
+head(mbta5)
+
+# Split month column into month and year: mbta6
+mbta6 <- separate(mbta5, month, c("month", "year"), sep = "-")
+
+# View the head of mbta6
+head(mbta6)
+# View a summary of mbta6
+summary(mbta6)
+
+# Generate a histogram of Boat column
+hist(mbta6$Boat)
+
+# Find the row number of the incorrect value: i
+i <- which(mbta6$Boat == 40)
+
+# Replace the incorrect value with 4
+mbta6$Boat[i] <- 4
+
+# Generate a histogram of Boat column
+hist(mbta6$Boat)
+
+# Look at Boat and Trackless Trolley ridership over time (don't change)
+ggplot(mbta_boat, aes(x = month, y = thou_riders, col = mode)) +  geom_point() + 
+  scale_x_discrete(name = "Month", breaks = c(200701, 200801, 200901, 201001, 201101)) + 
+  scale_y_continuous(name = "Avg Weekday Ridership (thousands)")
+
+# Look at all T ridership over time (don't change)
+ggplot(mbta_all, aes(x = month, y = thou_riders, col = mode)) + geom_point() + 
+  scale_x_discrete(name = "Month", breaks = c(200701, 200801, 200901, 201001, 201101)) +  
+  scale_y_continuous(name = "Avg Weekday Ridership (thousands)")
