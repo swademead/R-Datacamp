6ac57dfdd6b2dcedef804321eec07f711b31b245
swademead <swademead@gmail.com>
2018-02-28 12:48:00 -0500

    added chapter 4

--- oopinR.r
+++ oopinR.r
@@ -265,3 +265,120 @@ a_microwave_oven$power_level_watts <- 1600
 
 # Set the power level to 400 watts
 a_microwave_oven$power_level_watts <- 400
+
+#CHAPTER 4
+# Explore the microwave oven class
+microwave_oven_factory
+
+# Define a fancy microwave class inheriting from microwave oven
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory)
+
+  # Explore microwave oven classes
+microwave_oven_factory
+fancy_microwave_oven_factory
+
+# Instantiate both types of microwave
+a_microwave_oven <- microwave_oven_factory$new()
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+
+# Get power rating for each microwave
+microwave_power_rating <- a_microwave_oven$power_rating_watts
+fancy_microwave_power_rating <- a_fancy_microwave$power_rating_watts
+
+# Verify that these are the same
+identical(microwave_power_rating, fancy_microwave_power_rating)
+
+# Cook with each microwave
+a_microwave_oven$cook(1)
+a_fancy_microwave$cook(1)
+
+# Explore microwave oven class
+microwave_oven_factory
+
+# Extend the class definition
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  # Add a public list with a cook baked potato method
+  public = list(
+    cook_baked_potato = function() {
+      self$cook(3)
+    }
+  )
+)
+
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+
+# Call the cook_baked_potato() method
+a_fancy_microwave$cook_baked_potato()
+
+# Explore microwave oven class
+microwave_oven_factory
+
+# Update the class definition
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  # Add a public list with a cook method
+  public = list(
+    cook = function(time_seconds) {
+      super$cook(time_seconds)
+      message("Enjoy your dinner!")
+    }
+  )
+)
+
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+
+# Call the cook() method
+a_fancy_microwave$cook(1)
+
+# Expose the parent functionality
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  public = list(
+    cook_baked_potato = function() {
+      self$cook(3)
+    },
+    cook = function(time_seconds) {
+      super$cook(time_seconds)
+      message("Enjoy your dinner!")
+    }
+  ),
+  # Add an active element with a super_ binding
+  active = list(
+    super_ = function() super
+  )
+)
+
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+
+# Call the super_ binding
+a_fancy_microwave$super_
+
+# Explore other microwaves
+microwave_oven_factory
+fancy_microwave_oven_factory
+
+# Define a high-end microwave oven class
+high_end_microwave_oven_factory <- R6Class(
+"HighEndMicrowaveOven",
+inherit = fancy_microwave_oven_factory,
+  public = list(
+    cook = function(time_seconds) {
+      super$super_$cook(time_seconds)
+      message(ascii_pizza_slice)
+    }
+  )
+)
+# Instantiate a high-end microwave oven
+a_high_end_microwave <- high_end_microwave_oven_factory$new()
+
+# Use it to cook for one second
+a_high_end_microwave$cook(1)
